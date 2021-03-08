3f5b547a529635c01ac74bafe4e7405fef3bdb5e
Fabio TENORIO <fabiovalho@gmail.com>
2021-03-08 23:26:29 +0100

    job09 vraiment fini cette fois

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -6,26 +6,16 @@ ascendant ou décroissant, selon le paramètre passé, puis retourner le
 tableau.
 */
 
-var example = [3, 8, 4, 2, 10, 9, 16, 99, 7, 89, 12, 35, 48, 22, 52, 13, 17]
+var example = [3, 8, 4, 2, 10, 9, 16, 99, 7, 89, 12, 35, 48, 22, 52, 13, 17];
 
 function pickMax(numbers) {
-    var max = numbers[0];
+    var max = [];
     for (var i=0;i<numbers.length;i++) {
         if (max<numbers[i]) {
             max=numbers[i];
         }
     }
     return max;
-}
-
-function pickMin(numbers) {
-    var min = numbers[0];
-    for (var i=0;i<numbers.length;i++) {
-        if (min>numbers[i]) {
-            min=numbers[i];
-        }
-    }
-    return min;
 }
 
 function tri (numbers, order) {
@@ -49,5 +39,10 @@ function tri (numbers, order) {
         return asc;
     }
 }
-
-document.write(tri (example, "asc"));
+document.write("<p>voici les nombres à ranger:</p>");
+document.write (example + "<br>");
+document.write("<p>les voici en ordre ascendente:</p>");
+document.write(tri (example, "asc") + "<br>");
+example = [3, 8, 4, 2, 10, 9, 16, 99, 7, 89, 12, 35, 48, 22, 52, 13, 17];
+document.write("<p>les voici en ordre descendente:</p>");
+document.write(tri (example, "desc") + "<br>");
