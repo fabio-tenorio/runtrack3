7a443ae4448f9b9396b84af387c54e6596726dba
Fabio TENORIO <fabiovalho@gmail.com>
2021-03-08 10:15:59 +0100

    job01 terminé

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,7 +1,14 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>runtrack3 | job01</title>
+    <script>
+        alert("Hello Javascript!");
+    </script>
 </head>
 <body>
+    
 </body>
 </html>
