d314547b9152cf7716dc96dc278bd84c11dbc577
Fabio TENORIO <fabiovalho@gmail.com>
2021-03-10 23:52:05 +0100

    job02 du jour03 fini

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -4,16 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Runtrack 3 - Jour 3 - Job 1</title>
 </head>
-<body>
-    <p class="text">le $ est un bon élément monétaire.
-        Le # de twitter est une bonne I D.
-        Il faudra faire le point avec la
-        classe pour cacher cet élément.</p>
-    <br>
-    <button type="button" id="button">show</button>
-    <button type="button" id="hide">hide</button>
+<body class="container">
+    <div class="row" id="melangees">
+    </div>
+    <button class="btn btn-warning" type="button" id="button">melanger</button>
+    <div class="row" id="rangees">
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="script.js"></script>
 </body>
--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,3 +1,121 @@
-$(".text").css("display", "none");
-$("#button").click(function() {$(".text").css("display", "initial")});
-$("#hide").click(function() {$(".text").css("display", "none")});
+$("body").css("display", "flex");
+$("body").css("flex-direction", "column");
+$("body").css("align-items", "center");
+$("body").css("width", "100%");
+$("body").css("height", "200px");
+$("div").css("border", "3px solid black");
+$("div").css("margin", "35px 0");
+
+for(let i=1; i<7; i++) {    
+    $("#melangees").append("<img id='arc" + i + "' src='arc" + i + ".png' alt=''>");
+}
+
+$("img").each(function () {
+    $(this).css("width", "80px");
+});
+
+$("#rangees").after("<span>vous avez gagné</span>");
+$("span").css("display", "hide");
+
+function aleaNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+let imgOrder = [0, 1, 2, 3, 4, 5];
+let order = [];
+
+function replace(a,b) {
+    $.each(imgOrder, function(a, b) {
+    $('#arc'+a).after($('#arc'+b));
+    });    
+}
+
+function mix() {
+    while (imgOrder.length>0) {
+        let imgNum = aleaNum();
+        if (imgNum<7) {
+            $('#arc'+imgNum).appendTo('#melangees');
+            imgOrder.splice(imgNum-1,1);
+        }
+    }
+    imgOrder = [0, 1, 2, 3, 4, 5];
+    order = [];
+    $("span").css("display", "none");
+}
+
+mix();
+
+$("#button").click(mix);
+
+$('#arc1').click(function() {
+    var arc1 = 0;
+    order.push(arc1);
+    console.log(order);
+    $('#arc1').appendTo('#rangees');
+    match();
+});
+$('#arc2').click(function() {
+    var arc2 = 1;
+    order.push(arc2);
+    console.log(order);
+    $('#arc2').appendTo('#rangees');
+    match();
+});
+$('#arc3').click(function() {
+    var arc3 = 2;
+    order.push(arc3);
+    console.log(order);
+    $('#arc3').appendTo('#rangees');
+    match();
+});
+$('#arc4').click(function() {
+    var arc4 = 3;
+    order.push(arc4);
+    console.log(order);
+    $('#arc4').appendTo('#rangees');
+    match();
+});
+$('#arc5').click(function() {
+    var arc5 = 4;
+    order.push(arc5);
+    console.log(order);
+    $('#arc5').appendTo('#rangees');
+    match();
+});
+$('#arc6').click(function() {
+    var arc6 = 5;
+    order.push(arc6);
+    console.log(order);
+    $('#arc6').appendTo('#rangees');
+    match();
+});
+
+$("img").each(function () {
+    console.log($(this).attr("id"));
+});
+
+console.log(order.length);
+
+function match () {
+    if (order.length===6) {
+        console.log("terminou!");
+        console.log(order);
+        console.log(imgOrder);
+        var check = true;
+        for (var value=0;value<7;value++) {
+            if (order[value]!==imgOrder[value]) {
+                check = false;
+            }
+        }
+        if (check === true) {
+            $("#rangees").after("<span>vous avez gagné</span>");
+            $("span").css("display", "box");
+            $("span").addClass("btn btn-success");
+        } else {
+            $("#rangees").after("<span>vous avez perdu</span>");
+            $("span").css("display", "box");
+            $("span").addClass("btn btn-danger");
+        }
+        order = [];
+    }
+}
