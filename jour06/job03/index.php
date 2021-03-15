04a52e78492d5d7991343f8a6b2e30489cd7a57e
Fabio TENORIO <fabiovalho@gmail.com>
2021-03-15 16:09:41 +0100

    job04 du jour06 terminé

--- jour06/job03/index.php
+++ jour06/job03/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="#"/>
     <link rel="stylesheet" href="style.css">
-    <title>Runtrack 2 - jour 06 - job02</title>
+    <title>Runtrack 2 - jour 06 - job03</title>
 </head>
 <body>
     <section class="section"></section>
